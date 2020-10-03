98e78c3b8c0c9d5f304d656794fdb32a5acdfc0c
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-03 05:38:42 +0530

    [abhishek]:check for valid last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,12 +10,24 @@ public class UserRegistration {
 		String firstName = sc.next();
 		String pattern = "[A-Z]{1}[a-zA-Z]{2,}";
 
-		Pattern p = Pattern.compile(pattern);
-		Matcher m = p.matcher(firstName);
-		if (m.matches())
+		Pattern pFirstName = Pattern.compile(pattern);
+		Matcher mFirstName = p.matcher(firstName);
+		if (mFirstName.matches())
 			System.out.println("User First Name is valid ");
 		else
 			System.out.println("User First Name is invalid");
+
+		// uc2 check for last name
+		System.out.println("Enter the Last Name");
+		String lastName = sc.next();
+		String patternLastName = "[A-Z]{1}[a-zA-Z]{2,}";
+
+		Pattern pLastName = Pattern.compile(pattern);
+		Matcher mLastName = pLastName.matcher(lastName);
+		if (mLastName.matches())
+			System.out.println("User Last Name is valid ");
+		else
+			System.out.println("User Last Name is invalid");
 		sc.close();
 	}
 
